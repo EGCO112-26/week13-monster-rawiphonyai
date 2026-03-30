cd29afd53f5d8e42cec995ad605a65b35e85d49e
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-03-30 08:36:28 +0000

    Week 12 Monster

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
-    return 0;
+    monster *p;
+
+    monster m1("Gozaro",210,15);
+    monster m2("Mercury"),m4("Mars",210);
+    monster m3("Venus");
+    monster x[5]={{"Saturn",250},{"Polaris"},{"Jupiter",210,20},{"Gemini"}};
+    p=new monster("Uranus",50,50);
+    delete p; //destructor p
+    return 0; //destructor x,m2,m1,m3,m4
+
 }
--- monster.h
+++ monster.h
@@ -13,9 +13,27 @@ private:
     int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string, int=10,int=1);
+    //Atl+126
+    ~monster();
 };
+
+monster::~monster(){
+    cout<<"BYE BYE "<< name<<" hp: "<<hp<<endl;
+
+}
+
+monster::monster(string n="Vivi",int h,int p){
+    name=n;
+    if(hp>300) hp=300;
+    hp=h;
+    if(potion>20) potion=20;
+    potion=p;
+
+    cout<<"Monster "<<name <<" MaLeaw Naja!!! "<<endl;
+
+}
 
 
 #endif /* monster_h */
